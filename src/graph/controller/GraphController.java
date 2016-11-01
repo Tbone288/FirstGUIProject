422187845fd74b6f2f610b2ee90e3dd62682d866
Tbone288 <todo3858@740s-pl-1176.local>
2016-11-01 12:25:51 -0600

    added 3 helper methods and firstFrame class

--- src/graph/controller/GraphController.java
+++ src/graph/controller/GraphController.java
@@ -1,8 +1,16 @@
 package graph.controller;
 
+import graph.view.FirstFrame;
+
 public class GraphController
 {
 	
+	private FirstFrame appFrame;
+	
+	public GraphController()
+	{
+		this.appFrame = new FirstFrame(this);
+	}
 	
 	public void start()
 	{
--- src/graph/view/FirstFrame.java
+++ src/graph/view/FirstFrame.java
@@ -1,6 +1,26 @@
 package graph.view;
 
-public class FirstFrame
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import graph.controller.GraphController;
+
+public class FirstFrame extends JFrame
 {
 
+	
+	private GraphController baseController;
+	
+	public FirstFrame(GraphController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		
+		this.setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setSize(new Dimension(600,400));
+		this.setVisible(true);
+	}
 }
